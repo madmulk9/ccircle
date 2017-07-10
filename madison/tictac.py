1ae8049a4df35886e9ad0c6733f9da960ad1dad0
madmulk <madisonfmulkey@gmail.com>
2017-07-10 10:34:44 -0500

    finish tic tac toe project

--- madison/tictac.py
+++ madison/tictac.py
@@ -5,12 +5,16 @@ window = ccircle.Window("issa tic tac too", 324, 346)
 class Square:
     def __init__(self, vPos, hPos):
         self.claimed = False
+        self.xClaimed = False
+        self.oClaimed = False
         self.size = 100
-        self.colors = [0.0, 0.0, 0.0]
+        self.colors = [1.0, 1.0, 1.0]
+        self.initVPos = vPos
         self.vPos = 0
+        self.initHPos = hPos
         self.hPos = 0
         self.drawHitbox(vPos, hPos)
-        print("initialized at " + hPos + ", " + vPos)
+        print("initialized at " + self.initHPos + ", " + self.initVPos)
 
     def drawHitbox(self, vPos, hPos):
         if vPos == 't':
@@ -62,19 +66,30 @@ class Square:
             raise Exception("No vertical position defined when instantiating class Square.")
 
     def updateHitbox(self):
-        if not self.claimed:
-            self.colors = [0.0, 0.0, 0.0]
-        else:
-            self.colors = [1.0, 1.0, 1.0]
         window.drawRect(self.hPos, self.vPos, self.size, self.size, *self.colors)
+        if self.xClaimed:
+            window.drawLine(self.hPos + 10, self.vPos + 10, self.hPos + 90, self.vPos + 90, 4, 0, 0, 0)
+            window.drawLine(self.hPos + 10, self.vPos + 90, self.hPos + 90, self.vPos + 10, 4, 0, 0, 0)
+        if self.oClaimed:
+            window.drawCircle(self.hPos + 50, self.vPos + 50, 45, 0, 0, 0)
+            window.drawCircle(self.hPos + 50, self.vPos + 50, 41, 1, 1, 1)
 
     def onClick(self, mX, mY):
         if ccircle.isMouseDown('left') and ((mX >= self.hPos and mX <= self.hPos + self.size)
                                             and (mY >= self.vPos and mY <= self.vPos + self.size)):
-            self.claimed = True
+            if not self.claimed:
+                self.xClaimed = True
+                self.claimed = True
         elif ccircle.isMouseDown('right') and ((mX >= self.hPos and mX <= self.hPos + self.size)
                                             and (mY >= self.vPos and mY <= self.vPos + self.size)):
-            self.claimed = False
+            if not self.claimed:
+                self.oClaimed = True
+                self.claimed = True
+
+    def reset(self):
+        self.claimed = False
+        self.xClaimed = False
+        self.oClaimed = False
 
 squares = [Square('t', 'l'), Square('t', 'c'), Square('t', 'r'),
            Square('m', 'l'), Square('m', 'c'), Square('m', 'r'),
@@ -84,7 +99,12 @@ squares = [Square('t', 'l'), Square('t', 'c'), Square('t', 'r'),
 
 while window.isOpen():
     window.clear(1, 1, 1)
+
     mouseX, mouseY = window.getMousePos()
+
+    if ccircle.isKeyDown('r'):
+        for i in range(len(squares)):
+            squares[i].reset()
     for i in range(len(squares)):
         squares[i].onClick(mouseX, mouseY)
         squares[i].updateHitbox()
@@ -93,7 +113,5 @@ while window.isOpen():
     window.drawLine(0, 206, 308, 206, 4, .2, .2, .2)
     window.drawLine(102, 0, 102, 308, 4, .2, .2, .2)
     window.drawLine(206, 0, 206, 308, 4, .2, .2, .2)
-    # detect clicks on square object
-    # update with the cross or circle
 
     window.update()
