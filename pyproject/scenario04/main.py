d906bbdf28b925aed3b72c02d43e5422403919c1
madmulk <madisonfmulkey@gmail.com>
2017-07-11 16:05:02 -0500

    Local main.py changes for server scenario

--- pyproject/scenario04/main.py
+++ pyproject/scenario04/main.py
@@ -4,11 +4,33 @@ import connection
 con = connection.create()
 con.send('set_name', {'name': 'weeeeeeeeeee'})
 args = {
-    'vx': 15,
-    'vy': 50
+    'vx': 0,
+    'vy': 0
 }
 
-con.send('set_velocity', args)
+window = ccircle.Window("control window")
+
+while (window.isOpen()):
+    window.clear(0.2, 0.2, 0.2)
+
+    if ccircle.isKeyDown('d'):
+        args['vx'] = 50
+    elif ccircle.isKeyDown('a'):
+        args['vx'] = -50
+    else:
+        args['vx'] = 0
+
+    if ccircle.isKeyDown('w'):
+        args['vy'] = -50
+    elif ccircle.isKeyDown('s'):
+        args['vy'] = 50
+    else:
+        args['vy'] = 0
+
+    con.send('set_velocity', args)
+
+
+
 
 # Write code to make money and kill the evil cat!
 # See readme.txt !
