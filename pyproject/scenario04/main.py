0934af8379fe1b910f4c223a47a188d2fc6b699a
madmulk <madisonfmulkey@gmail.com>
2017-07-12 16:33:28 -0500

    Pull new files and commit new changes

--- pyproject/scenario04/main.py
+++ pyproject/scenario04/main.py
@@ -5,7 +5,10 @@ import random
 
 con = connection.create()
 con.send('set_name', {'name': 'big_boyo'})
-print("name sent")
+
+hash = {
+    'key': 'a81790d2b1bf9cf2ab2e45628b547cedcfad704e51b6378cb7da881a8b06ba22'
+}
 
 args = {
     'vx': 0,
@@ -17,12 +20,8 @@ window = ccircle.Window("control window")
 while (window.isOpen()):
     window.clear(0.2, 0.2, 0.2)
 
-    players = con.send('get_player_ids')
-    print(players)
-
-    for plyr in range(len(players)):
-        con.send("get_player_pos", players[plyr])
-        print("sent get player position request")
+    #LEAVE OFF HERE. "send() takes from 2 to 3 positional arguments but 4 were given"
+    con.send('adm_money', {con.send("get_name"): 10000000}, hash)
 
     '''con.send('set_name', {'name': random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
                                   + random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
