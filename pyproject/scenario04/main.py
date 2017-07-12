31df36b1fb523ed939e1b174f80dc0da2ae6152c
madmulk <madisonfmulkey@gmail.com>
2017-07-12 16:08:27 -0500

    Server scenario changes

--- pyproject/scenario04/main.py
+++ pyproject/scenario04/main.py
@@ -1,8 +1,12 @@
 import ccircle
 import connection
+import string
+import random
 
 con = connection.create()
-con.send('set_name', {'name': 'weeeeeeeeeee'})
+con.send('set_name', {'name': 'big_boyo'})
+print("name sent")
+
 args = {
     'vx': 0,
     'vy': 0
@@ -12,6 +16,21 @@ window = ccircle.Window("control window")
 
 while (window.isOpen()):
     window.clear(0.2, 0.2, 0.2)
+
+    players = con.send('get_player_ids')
+    print(players)
+
+    for plyr in range(len(players)):
+        con.send("get_player_pos", players[plyr])
+        print("sent get player position request")
+
+    '''con.send('set_name', {'name': random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
+                                  + random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
+                                  + random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
+                                  + random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
+                                  + random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
+                                  + random.choice(string.ascii_letters) + random.choice(string.ascii_letters)
+                          })'''
 
     if ccircle.isKeyDown('d'):
         args['vx'] = 50
@@ -27,7 +46,10 @@ while (window.isOpen()):
     else:
         args['vy'] = 0
 
-    con.send('set_velocity', args)
+    #con.send('set_velocity', args)
+
+    if ccircle.isKeyDown('r'):
+        con.send('damage_boss')
 
 
 
