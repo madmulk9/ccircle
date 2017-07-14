1292d2c2f1eec6e6220de3cd2614bc9205cf07b4
madmulk <madisonfmulkey@gmail.com>
2017-07-14 16:25:11 -0500

    Sound file changes

--- pyproject/lab_audio_synthesis/main.py
+++ pyproject/lab_audio_synthesis/main.py
@@ -13,8 +13,27 @@ def sawtooth(t, f):
 def sine(t, f):
     return sin(2.0 * pi * t * f)
 
-for i in range(44100):
+def signum(int):
+    if int > 0: return 1
+    elif int < 0: return -1
+    elif int == 0: return 0
+    else: return int
+
+def square(t, f):
+    return signum(sin(t * f * 2 * pi))
+
+duration = 3
+
+for i in range(44100 * duration):
     t = i / 44100
-    mysound.addSample(sine(t, 440))
+    if t < 0.36:
+        mysound.addSample(square(t, 220))
+    elif t < 0.72:
+        mysound.addSample(square(t, 207.6523487900))
+    elif t < 1.08:
+        mysound.addSample(square(t, 195.9977179909))
+    elif t < 3.0:
+        mysound.addSample(square(t, 184.9972113558))
+
 mysound.play()
-time.sleep(1)
+time.sleep(duration)
